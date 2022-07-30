9264a5e3a9057bd3ee4b3d81882b7b3bf0e3edd4
chen <1542515276@qq.com>
2022-07-30 13:48:27 +0800

    点餐项目第二次提交到本地，增删查改基本功能实现

--- src/http/api.js
+++ src/http/api.js
@@ -1,7 +1,9 @@
 import * as login from "@/http/modules/login";
 import * as menu from "@/http/modules/menu";
+import * as order from "@/http/modules/order";
 
 export default {
     login,
-    menu
+    menu,
+    order
 }
--- src/views/order/Order.vue
+++ src/views/order/Order.vue
@@ -1,15 +1,441 @@
 <template>
+
   <div>
-    订单管理
+    <n-button @click="getOrder(1)">
+      查看当前订单
+    </n-button>
+    <n-button @click="getOrder(2)">
+      查看历史订单
+    </n-button>
+    <n-button @click="deleteOrder">
+      删除订单
+    </n-button>
+    <n-button @click="insertOrder">
+      添加订单
+    </n-button>
+
+    <n-popconfirm
+            @positive-click="handlePositiveClick"
+            @negative-click="handleNegativeClick"
+    >
+      <template #trigger>
+        <n-button type="info" id="saveButton">
+          保存数据
+        </n-button>
+      </template>
+      确认保存数据吗？
+    </n-popconfirm>
+    <n-p> 你选中了ID为 {{ checkedRowKeysRef }} ，共计{{checkedRowKeysRef.length}}条订单。</n-p>
+    <n-data-table
+            :columns="columns"
+            :data="data"
+            :pagination="pagination"
+            :max-height="550"
+            :scroll-x="1700"
+            virtual-scroll
+            :row-class-name="rowClassName"
+            flex-height
+            @update:checked-row-keys="handleCheck"
+            ellipsis="EllipsisProps"
+    >
+      <n-checkbox v-model:checked="value"/>
+    </n-data-table>
   </div>
 </template>
 
-<script>
-export default {
-    name: "Order"
-}
+<script setup>
+    import {getCurrentInstance, h, onUpdated, ref} from "vue";
+    import {NInput, NSelect} from "naive-ui";
+    import {ElNotification} from 'element-plus';
+
+    const {proxy} = getCurrentInstance();
+    let data = ref();
+    let getOrderStatus = 0;
+    let insertCount = 0;
+    const warn = message => {
+        ElNotification({
+            title: '叮！',
+            message: h('i', {style: 'color: teal'}, message),
+        })
+    }
+    const checkedRowKeysRef = ref([]);
+    const handleCheck = (rowKeys) => {
+        if (rowKeys.length > 3)
+            warn("一次最多只能选择三条数据！")
+        else
+            checkedRowKeysRef.value = rowKeys;
+    }
+
+    function handlePositiveClick() {
+        //确认
+        saveData()
+    }
+
+    function handleNegativeClick() {
+        //取消
+    }
+
+    let modifyingDataArr = []
+    const saveData = () => {
+        console.log(modifyingDataArr);
+        proxy.$api.order.modifyOrder("/order/modifyOrder",
+            // {
+            //     order: modifyingDataArr,
+            // }
+            modifyingDataArr
+        ).then(
+            res => {
+                if (!res)
+                    warn("修改失败！")
+                else warn("修改成功！")
+            },
+            err => {
+                warn(err.reason)
+            })
+    }
+
+    const insertOrder = () => {
+        if(getOrderStatus === 0)
+            warn("请先查询订单再添加！")
+        else{
+            let temp = [];
+            // if (insertCount > 0) {
+            //     warn("一次只能添加一条数据！");
+            //     return;
+            // }
+            // insertCount += 1;
+            // data.value.push({
+            //     // startTime:,
+            //     // list:,
+            //     // amount:,
+            //     //
+            //     //
+            // })
+            proxy.$api.order.insertOrder("/order/insert",
+                {
+                    id: 0,
+                    startTime: '',
+                    list: '',
+                    amount: 0,
+                    orderStatus: 0,
+                    message: '',
+                    customerId: getOrderStatus
+                }
+            ).then(
+                res => {
+                    if (!res) {
+                        warn("添加失败！")
+                        // insertCount = 0
+                    } else {
+                        for (let j = 0; j < res.length; j++) {
+                            // if (res[j].orderStatus === -1) status = "已失败"
+                            // else if (res[j].orderStatus === 0) status = "进行中"
+                            // else status = "已完成"
+                            temp.push({
+                                key: res[j].id,
+                                list: res[j].list,
+                                startTime: res[j].startTime,
+                                customerId: res[j].customerId,
+                                message: res[j].message,
+                                orderStatus: res[j].orderStatus,
+                                amount: res[j].amount,
+                                id: res[j].id
+                            })
+                        }
+                        data.value = temp
+                        warn("添加成功！")
+                        // insertCount = 0
+                    }
+                },
+                err => {
+                    warn(err.reason)
+                })
+        }
+
+    }
+    const createColumns = () => [
+        {
+            type: "selection",
+        },
+        {
+            title: "订单ID",
+            key: "id",
+            render(row, index) {
+                return h(NInput, {
+                    value: row.id,
+                    // disabled:true,
+                    onUpdateValue(v) {
+                        warn("不支持修改订单ID！")
+                        // data.value[index].id = v;
+                    }
+                });
+            }
+        },
+        {
+            title: "创建时间",
+            key: "startTime",
+            render(row, index) {
+                return h(NInput, {
+                    value: row.startTime,
+                    onUpdateValue(v) {
+                        data.value[index].startTime = v;
+                        let temp = data.value[index]
+                        delete temp.key
+                        modifyingDataArr[row.id] = temp;
+                        // console.log(modifyingDataArr)
+                    }
+                });
+            }
+        },
+        {
+            title: "订单内容",
+            key: "list",
+            render(row, index) {
+                return h(NInput, {
+                    value: row.list,
+                    onUpdateValue(v) {
+                        data.value[index].list = v;
+                        let temp = data.value[index]
+                        delete temp.key
+                        modifyingDataArr[row.id] = temp;
+                        // console.log(modifyingDataArr)
+                    }
+                });
+            }
+        },
+        {
+            title: "总金额",
+            key: "amount",
+            render(row, index) {
+                return h(NInput, {
+                    value: row.amount,
+                    onUpdateValue(v) {
+                        data.value[index].amount = v;
+                        let temp = data.value[index]
+                        delete temp.key
+                        modifyingDataArr[row.id] = temp;
+                        // console.log(modifyingDataArr)
+                    }
+                });
+            }
+        },
+        {
+            title: "订单状态",
+            key: "orderStatus",
+            render(row, index) {
+                return h(NSelect, {
+                    options: [
+                        {
+                            value: 1,
+                            label: "已完成"
+                        },
+                        {
+                            value: -1,
+                            label: "已失败"
+                        },
+                        {
+                            value: 0,
+                            label: "进行中"
+                        },
+                    ],
+                    value: row.orderStatus,
+                    onUpdateValue(v) {
+                        data.value[index].orderStatus = v;
+                        let temp = data.value[index]
+                        delete temp.key
+                        modifyingDataArr[row.id] = temp;
+                        // console.log(modifyingDataArr)
+                    }
+                });
+            }
+        },
+        {
+            title: "备注",
+            key: "message",
+            render(row, index) {
+                return h(NInput, {
+                    value: row.message,
+                    onUpdateValue(v) {
+                        data.value[index].message = v;
+                        let temp = data.value[index]
+                        delete temp.key
+                        modifyingDataArr[row.id] = temp;
+                        // console.log(modifyingDataArr)
+                    }
+                });
+            }
+        },
+        {
+            title: "顾客ID",
+            key: "customerId",
+            render(row, index) {
+                return h(NInput, {
+                    value: row.customerId,
+                    onUpdateValue(v) {
+                        data.value[index].customerId = v;
+                        let temp = data.value[index]
+                        delete temp.key
+                        modifyingDataArr[row.id] = temp;
+                        // console.log(modifyingDataArr)
+                    }
+                });
+            }
+        }
+    ];
+
+    onUpdated(() => {
+
+    })
+    const deleteOrder = () => {
+        let temp = [];
+        proxy.$api.order.deleteOrder("/order/deleteOrder", {
+            orderId: checkedRowKeysRef.value,
+            status: getOrderStatus
+        }).then(
+            res => {
+                for (let j = 0; j < res.length; j++) {
+                    // if (res[j].orderStatus === -1) status = "已失败"
+                    // else if (res[j].orderStatus === 0) status = "进行中"
+                    // else status = "已完成"
+                    temp.push({
+                        key: res[j].id,
+                        list: res[j].list,
+                        startTime: res[j].startTime,
+                        customerId: res[j].customerId,
+                        message: res[j].message,
+                        orderStatus: res[j].orderStatus,
+                        amount: res[j].amount,
+                        id: res[j].id
+                    })
+                }
+                data.value = temp
+                checkedRowKeysRef.value.splice(0,checkedRowKeysRef.value.length)
+            },
+            err => {
+                warn(err.reason)
+            })
+
+    }
+    const rowClassName = (row) => {
+        console.log(row)
+        if (row.orderStatus === -1 || row.orderStatus === "-1") {
+            return "too-old";
+        }
+        return "";
+    }
+    let columns = createColumns();
+
+    const getOrder = (i) => {
+        getOrderStatus = i
+        let temp = [];
+        let status = ''
+        if (i === 1)
+            proxy.$api.order.getOrder("/order/getCurrentOrder").then(
+                res => {
+                    for (let j = 0; j < res.length; j++) {
+                        // if (res[j].orderStatus === -1) status = "已失败"
+                        // else if (res[j].orderStatus === 0) status = "进行中"
+                        // else status = "已完成"
+                        temp.push({
+                            key: res[j].id,
+                            list: res[j].list,
+                            startTime: res[j].startTime,
+                            customerId: res[j].customerId,
+                            message: res[j].message,
+                            orderStatus: res[j].orderStatus,
+                            amount: res[j].amount,
+                            id: res[j].id
+                        })
+                    }
+                    data.value = temp
+                },
+                err => {
+                    warn(err.reason)
+                })
+        else proxy.$api.order.getOrder("/order/getHistoryOrder").then(
+            res => {
+                for (let j = 0; j < res.length; j++) {
+                    // if (res[j].orderStatus === -1) status = "已失败"
+                    // else if (res[j].orderStatus === 0) status = "进行中"
+                    // else status = "已完成"
+                    temp.push({
+                        key: res[j].id,
+                        list: res[j].list,
+                        startTime: res[j].startTime,
+                        customerId: res[j].customerId,
+                        message: res[j].message,
+                        orderStatus: res[j].orderStatus,
+                        amount: res[j].amount,
+                        id: res[j].id
+                    })
+                }
+                data.value = temp
+            },
+            err => {
+                warn(err.reason)
+            })
+    }
+
+    let pagination = {
+        pageSize: 10
+    }
+
+
 </script>
 
-<style>
+<style scoped>
+  .n-data-table {
+    margin: 10px 30px;
+    width: 1200px;
+    height: 450px;
+    box-shadow: 25px 25px 40px -23px grey;
+    background-color: #ffffff;
+  }
 
+  .n-p {
+    margin-left: 30px;
+    position: absolute;
+    right: 200px;
+    top: 75px;
+    z-index: 7;
+  }
+
+  .n-button {
+    width: 120px;
+    margin: 5px 5px 5px 30px;
+  }
+
+  #saveButton {
+    width: 120px;
+    float: right;
+    margin-right: 40px;
+  }
+
+  :deep(.too-old td) {
+    background-color: #e3929a !important;
+    animation: fade 1200ms;
+  }
+
+  :deep(.age) {
+    background-color: rgba(0, 128, 0, 0.75) !important;
+  }
+
+  :deep(.too-old .age) {
+    background-color: rgba(0, 0, 128, 0.75) !important;
+  }
+
+
+  @keyframes fade {
+    from {
+      opacity: 1.0;
+    }
+    50% {
+      opacity: 0.0;
+    }
+    to {
+      opacity: 1.0;
+    }
+
+  }
 </style>
+
