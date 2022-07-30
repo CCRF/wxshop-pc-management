e1608b781ba5aef581e26c9d0dbcb41c5342f478
yejian <911617191@qq.com>
2022-07-30 13:47:30 +0800

    1.套餐路由的基本完善 2.在api.js中新加了SetMeal的一些东西 3.使用了element-plus，采用全局引入方式

--- src/http/api.js
+++ src/http/api.js
@@ -1,7 +1,8 @@
 import * as login from "@/http/modules/login";
 import * as menu from "@/http/modules/menu";
-
+import * as SetMeal from "@/http/modules/SetMeal"
 export default {
     login,
-    menu
+    menu,
+    SetMeal
 }
---
+++ src/http/modules/SetMeal.js
@@ -0,0 +1,18 @@
+import {get} from "@/http/axios";
+import {post} from "@/http/axios";
+
+export function findSetMeal(url,params){
+    return get(url,params);
+}
+
+export function deleteById(url,params){
+    return post(url,params);
+}
+
+export function addMeal(url,params){
+    return post(url,params)
+}
+
+export function findById(url,params){
+    return get(url,params)
+}
