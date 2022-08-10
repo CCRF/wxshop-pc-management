9e320eeb8305b197bd1778503003376b35d59f37
LXJ <526392973@qq.com>
2022-08-10 13:37:26 +0800

    整体界面优化

--- src/views/customer/Customer.vue
+++ src/views/customer/Customer.vue
@@ -1,8 +1,43 @@
 <template>
   <!-- 操作区域 -->
   <div>
-
+<!--    <el-upload action="http://localhost:8090/customer/upload">-->
+    <el-upload action="https://g1.glypro19.com/customer/upload">
+      <el-button type="primary">新增</el-button>
+    </el-upload>
+    <el-button type="primary" @click="this.insertVisible=true">新增商品</el-button>
+    <el-button type="primary" @click="test">test</el-button>
   </div>
+
+  <el-dialog v-model="insertVisible" title="新增商品" width="500px" @close="close" center>
+    <el-form label-width="100px" label-position="right"  >
+      <el-form-item label="套 餐 名 称">
+        <el-input v-model="addform.name" />
+      </el-form-item>
+      <el-form-item label="套 餐 名 称">
+        <el-input v-model="addform.nickName" />
+      </el-form-item>
+
+      <el-form-item label="图片上传">
+        <el-upload
+            action=''
+            ref="upload"
+            list-type="picture-card"
+            accept="image/jpeg,image/jpg,image/png"
+            :auto-upload="false"
+            :file-list="files"
+            :headers="importHeaders"
+        >
+          <el-button type="primary">上传图片</el-button>
+        </el-upload>
+      </el-form-item>
+
+
+      <el-form-item>
+        <el-button type="primary" @click='insertGoods'>提交</el-button>
+      </el-form-item>
+    </el-form>
+  </el-dialog>
 
   <!-- 顾客数据表格 -->
   <div>
@@ -26,14 +61,29 @@
 
 
 <script>
+import axios from 'axios';
+import qs from 'qs';
+
 export default {
   name: "Customer.vue",
   data(){
     return{
-      tableData:[]
+      tableData:[],
+      insertVisible:false,
+      addform:{
+        name:"",
+        nickName:""
+      },
+      files:[],
+      importHeaders: {
+        enctype: "multipart/form-data",
+      },
     }
   },
   mounted() {
+    let token=sessionStorage.getItem("token");
+    axios.defaults.headers.common['Authorization'] = token;
+
     this.$api.customer.getAllCustomer("/customer/getAll").then(res=>{
         console.log(res.data)
       this.tableData = res.data;
@@ -43,7 +93,30 @@ export default {
   },
   methods:{
     test(e) {
-      console.log(e);
+       axios.get("http://localhost:8090/customer/test")
+    },
+    insertGoods(){
+
+      // axios.post("http://localhost:8090/customer/upload",
+      //     {
+      //   "insertVisible":this.insertVisible,
+      //   "file":this.address
+      //     }, {
+      //       headers: {
+      //         'Content-Type': 'application/x-www-form-urlencoded'
+      //       }
+      //     })
+
+      const data = { 'file': this.files[0],'insertVisible':this.insertVisible ,'name':this.addform.name };
+      const options = {
+        method: 'POST',
+        // headers: { 'content-type': 'application/x-www-form-urlencoded' },
+        headers: { 'content-type': 'multipart/form-data' },
+
+        data: qs.stringify(data),
+        url:"http://localhost:8090/customer/upload",
+      };
+      axios(options)
     }
   }
 }
