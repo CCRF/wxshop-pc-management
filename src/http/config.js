e43a60a89d4943b77f0913a927b2fb402dfdec3f
yejian <911617191@qq.com>
2022-08-11 21:20:36 +0800

    不刷新界面，数据列表更新

--- src/http/config.js
+++ src/http/config.js
@@ -1,6 +1,6 @@
 export default {
-    baseUrl: 'http://localhost:8090',
-    // baseUrl: 'https://g1.glypro19.com/',
+    // baseUrl: 'http://localhost:8090',
+    baseUrl: 'https://g1.glypro19.com/',
     timeOut: 30000,
     headers: {
         'Content-Type': 'application/json;charset=UTF-8'
